5c13e853a220ee21a199ed47daeb8770cfffd146
Harol Paredes <harolparedesperez534@gmail.com>
2024-05-27 22:54:54 -0400

    Add files via upload

---
+++ Ejercicios_Capitulo2/Programa2.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    float PRO;
+    printf("Ingrese el promedio de alumno: ");
+    scanf("%f", &PRO);
+    if (PRO>= 6)
+        printf("\nAprobado");
+
+    return 0;
+}
