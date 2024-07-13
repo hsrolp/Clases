441d977830b7eae741267cf6f1ce3c25beace8cd
Harol Paredes <harolparedesperez534@gmail.com>
2024-07-13 11:16:25 -0400

    Add files via upload

---
+++ Capitulo 9/Programa9.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* El programa escribe caracteres en un archivo. */
+
+int main(void)
+{
+    char p1;
+    FILE *ar;
+    ar = fopen("arc.txt", "w");  
+    if (ar != NULL)
+    {
+        while ((p1 = getchar()) != '\n')
+        {
+            fputc(p1, ar);  
+        }
+        fclose(ar);
+    }
+    else
+    {
+        printf("No se puede abrir el archivo");
+    }
+    return 0;
+}
